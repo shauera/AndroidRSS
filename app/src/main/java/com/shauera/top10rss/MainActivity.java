b7bdc82eb7844b463470e0309434976d1670ece0
Avishalom Shauer <shauera@gmail.com>
2022-10-15 11:12:10 +0700

    Initial commit

---
+++ app/src/main/java/com/shauera/top10rss/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shauera.top10rss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
